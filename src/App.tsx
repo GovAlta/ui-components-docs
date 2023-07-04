23690df291a2ba9968fad180f169d29d405202e3
Chris Olsen <olsen.chris@gmail.com>
2023-07-04 12:30:42 -0600

    feat: add component Not Found page

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import ButtonPage from './routes/components/Button';
 import FormStepperPage from './routes/components/FormStepper';
 import CheckboxPage from './routes/components/Checkbox';
 import AllComponentsPage from './routes/components/AllComponents';
+import ComponentNotFoundPage from './routes/components/NotFound';
 import Root from './routes/root';
 
 import './index.css'
@@ -14,12 +15,14 @@ import './index.css'
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Root />}>
-      <Route path="components" element={<ComponentsPage />}>
+      <Route path="components" element={<ComponentsPage />} errorElement={<ComponentNotFoundPage />} >
         <Route index element={<AllComponentsPage />} />
         <Route path="button" element={<ButtonPage />} />
         <Route path="dropdown" element={<DropdownPage />} />
         <Route path="form-stepper" element={<FormStepperPage />} />
         <Route path="checkbox" element={<CheckboxPage />} />
+
+        <Route path="*" element={<ComponentNotFoundPage />} />
       </Route>
     </Route>
   )
