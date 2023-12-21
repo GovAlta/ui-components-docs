20ea397480206bb4a6a1fc678ccc4e4d43125de5
Thomas Jeffery <82968683+twjeffery@users.noreply.github.com>
2023-12-21 12:36:17 -0700

    chore: Update Button.tsx
    changing content to test commit and PR

--- src/routes/components/Button.tsx
+++ src/routes/components/Button.tsx
@@ -134,10 +134,7 @@ export default function ButtonPage() {
       <ComponentHeader
         name="Button"
         category={Category.INPUTS_AND_ACTIONS}
-        description="Buttons allow users to perform an action or to navigate to another page.
-        They have multiple styles for various needs, and are ideal for calling
-        attention to where a user needs to do something or so they can move
-        forward in a flow."
+        description="Carry out an important action or to navigate to another page."
         relatedComponents={[
           { link: "/components/icon-button", name: "Icon button" },
           { link: "/components/link", name: "Link" },
