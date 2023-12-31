ccfefd7a0207cee3491010daa1709d829c085043
Thomas Jeffery <82968683+twjeffery@users.noreply.github.com>
2023-12-31 14:38:00 -0700

    chore: content update
    token description updated

--- src/routes/design-tokens/shadow/Shadow.tsx
+++ src/routes/design-tokens/shadow/Shadow.tsx
@@ -15,8 +15,8 @@ export default function ShadowPage() {
   const tokens: Token[] = [
     {
       tokenName: "goa-shadow-modal",
-      value: "x=6,y=6,blur=6,spread=0, rgba(0,0,0,0,16)",
-      figmaUsage: "Drop Shadow/Small",
+      value: "x=6, y=6, blur=6, spread=0, rgba(0,0,0,0,16)",
+      figmaUsage: "Effect: Drop Shadow/Modal",
     },
   ];
   const { isDesktop } = useContext(DeviceWidthContext);
