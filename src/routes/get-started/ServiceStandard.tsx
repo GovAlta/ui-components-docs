dfd09bd4c6bd39f2fade4d98f12fdda22e365f64
Thomas Jeffery <82968683+twjeffery@users.noreply.github.com>
2024-03-18 13:46:37 -0600

    chore: revert

--- src/routes/get-started/ServiceStandard.tsx
+++ src/routes/get-started/ServiceStandard.tsx
@@ -1,4 +1,3 @@
-import { GoADetail, GoACallout } from "@abgov/react-components";
 export default function ServiceStandardPage() {
   return (
     <>
@@ -92,10 +91,6 @@ export default function ServiceStandardPage() {
         <h4>Digital products are suitable.</h4>
         <p>Usability testing should provide an understanding of how our digital product experiences deliver on user expectations 
           compared to other experiences users are familiar with.</p>
-        <GoADetails heading="Requirement">
-        Users can easily switch from one task to the next without confusion or frustration. 
-          Prompt users to explain or compare similar and preferred digital experiences.
-      </GoADetails>
         <p>Requirement: Users can easily switch from one task to the next without confusion or frustration. 
           Prompt users to explain or compare similar and preferred digital experiences.</p>
         
