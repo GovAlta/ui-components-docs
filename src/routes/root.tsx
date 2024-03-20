c9751e0004c2b0634b4d4a0a2e84fa4a9cb29dda
Vanessa Tran <Vanessa.M.Tran@gov.ab.ca>
2024-03-20 15:38:37 -0400

    fix(#1377): make app header collapse when screen size is less than 1140

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -44,7 +44,7 @@ export default function Root() {
             feedbackUrl="https://forms.microsoft.com/r/8Zp7zSJS6W"
             maxContentWidth={MAX_CONTENT_WIDTH}
           />
-          <GoAAppHeader heading="Design system" maxContentWidth={MAX_CONTENT_WIDTH} url={"/"}>
+          <GoAAppHeader heading="Design system" maxContentWidth={MAX_CONTENT_WIDTH} url={"/"} fullMenuBreakpoint={1140}>
             <Link to="/get-started">Get started</Link>
             <Link to="/patterns">Patterns and templates</Link>
             <Link to="/components">Components</Link>
