af4570fa287e20a039b45fe090ae7b67dcaaef5a
Thomas Jeffery <82968683+twjeffery@users.noreply.github.com>
2024-03-22 16:23:27 -0600

    chore: content update

--- src/routes/get-started/ServiceStandard.tsx
+++ src/routes/get-started/ServiceStandard.tsx
@@ -1,146 +1,65 @@
 export default function ServiceStandardPage() {
   return (
     <>
-      <h1>User experience standards</h1>
-      <h3>User experience standards for digital services at the Government of Alberta.</h3>
+      <h1>User experience guidelines</h1>
+      <h3>Digital products built for the Government of Alberta should look to comply with these guidelines to ensure a quality user experience for Albertans.</h3>
 
       <div className="max-width-72ch">
 
-        <h2>The strategy</h2>
         <p>
-          The Digital Design and Delivery’s User Experience Team is beginning the process of establishing 
-          a broader User Experience Strategy for the Government of Alberta. This strategy will be focused 
-          on the experience delivered to the public and/or staff through design and development of digital 
-          products.
-        </p>
-        <p>
-          As part of this strategy, resources are available for teams to conduct their own usability testing. 
-          The User Experience team is also available for consultation.
-        </p>
-    
-        <h2>Digital experience guidelines</h2>
-        <p>
-          These guidelines apply to all digital products across government, regardless of delivery method or 
-          platform for both public and employee experiences.
+          Usability testing is our preferred method to understand user needs as they relate to each guideline. Suitable usability testing includes:
         </p>
         
-        <h3>How do these guidelines relate to other standards and practices?</h3>
-        <p>
-          When the Government of Alberta begins to redesign or create a new digital service, we employ Service 
-          Design principles and methodologies. The Alberta Digital Strategy contains the standards that govern 
-          these outcomes. Once a service becomes supported by a digital product (working software), the 
-          Experience Guidelines specify quality measures for these digital experiences.
-        </p>
-        
-        <h3>Digital products are:</h3>
-        <ol>
-          <li><strong>Usable:&nbsp;</strong>Using human-centered design to understand users’ context of use, goals, tasks, and environments</li>
-          <li><strong>Suitable:&nbsp;</strong>Users can achieve their goals and complete tasks in a manner that is satisfactory and will be on par 
-            with outside modern digital products used in other aspects of users lives.</li>
-          <li><strong>Accessible:&nbsp;</strong>Digital products will be usable for the broadest range of users possible regardless of physical or 
-            cognitive limitations, literacy level, or technical capability.</li>
-          <li><strong>Comprehensible:&nbsp;</strong>Digital products are broadly comprehensible by the largest user base possible by presenting 
-            information in plain language, absent of organizational speak, industry jargon, and complex legalese.</li>
-          <li><strong>Device-agnostic:&nbsp;</strong>Users should not be limited by the device they choose to use or the technology they have 
-            access to when interacting with government.</li>
-          <li><strong>Compliant:&nbsp;</strong>The product must leverage the brand and convey authority and legitimacy. As a government asset, 
-            user expectations of safety and security must be understood and met with experiences that evoke a sense of trust.</li>
-          <li><strong>Lean:&nbsp;</strong>Interfaces are designed to be the least amount of function possible to meet the needs of users in 
-            a way that feels intuitive and can be operated without the need of outside help.</li>
-          <li><strong>Iterative:&nbsp;</strong>Products are continuously tested, resourced, and improved upon with users input and usage data 
-            to ensure lasting effectiveness, efficiency, and usability.</li>
-          <li><strong>Connected:&nbsp;</strong>Digital products will be designed for integration into the broader eco-system of the government 
-            reducing the need for new and separate destinations that fracture the experience.</li>
-          <li><strong>Responsive:&nbsp;</strong>Digital products will make available and clearly visible channels for technical support and 
-            allow users to submit feedback on product performance, bugs, or suggest improvements.</li>
-        </ol>
-        
-        <h3>Meeting the guidelines through usability testing</h3>
-        <p>
-          Usability testing is our primary process to ensure we are delivering digital experiences that meet the needs and expectations 
-          of users as well as our digital experience guidelines.
-        </p>
-        <p>
-          Usability testing success will help ensure we design the right product and will be determined by our ability to:
-        </p>
         <ul>
-          <li>Recruit a diverse set of users who will experience the problem or benefit the design is looking to improve upon</li>
-          <li>Recruit users varying in physical and cognitive limitations, literacy level, or technical capability</li>
-          <li>Include the users’ preferred choice of device during testing to understand the diversity of technology used to interact with government</li>
-          <li>Test continuously to understand changing needs and expectations as they arise</li>
-          <li>Ensure resource availability to act on test findings</li>
-          <li>Test entire process not just an application or task in isolation</li>
+          <li>Users of diverse demographic and behavioural backgrounds that will experience the problem or benefit the product is looking to improve upon.</li>
+          <li>Users with varying levels of physical and or cognitive limitations, literacy, and technical capability.</li>
+          <li>A diverse range of devices that reflect our users' preferred choice when interacting with government.</li>
+          <li>Repeated test cycles as products mature and user needs change over time.</li>
+          <li>Testing of complete services end-to-end, not just technology in isolation.</li>
         </ul>
+        
         <p>
           For more details on the process of usability testing, refer to our
           <a href="#" target="_blank">
-            Usability field guide.
+            usability field guide.
           </a>
         </p>
-        
-        <h3>User Experience Standards and Review</h3>
-        <p>The UX Team has established a Heuristic Self-Evaluation for teams to reference as they begin their usability testing planning
-          and implementation. This evaluation will be part of a User Experience Review process that the UX Team will be creating.</p>
-        <p>If you haven’t conducted a Heuristic Review before, please reach out to the the director of UX - <a href="#" target="_blank">Sheldon Bauld</a> for assistance.</p>
-        
-        <h4>Digital products are usable.</h4>
-        <p>Usability testing should focus on understanding user tasks and the efficiency in which users are able to perform actions and reach their goals.</p>
-        <p>Requirement: Identification of high priority user tasks and the content required for them to complete their tasks.</p>
-        
-        <h4>Digital products are suitable.</h4>
-        <p>Usability testing should provide an understanding of how our digital product experiences deliver on user expectations 
-          compared to other experiences users are familiar with.</p>
-        <p>Requirement: Users can easily switch from one task to the next without confusion or frustration. 
-          Prompt users to explain or compare similar and preferred digital experiences.</p>
-        
-        <h4>Digital products are accessible.</h4>
-        <p>Usability test participants should represent the diversity of users within the population the product is looking 
-          to serve.</p>
-        <p>Requirement: Recruit a diverse set of users varying in physical and cognitive limitations, 
-          literacy level, or technical capability within the population who will experience the problem or benefit the design is looking to 
-          improve upon, including internal users. Careful consideration has been placed on the diversity and inclusion of usability participants, 
-          such as socio-economic background, gender, and race.</p>
-        
-        <h4>Digital products are comprehensible.</h4>
-        <p>Usability testing should include validation that the language and information presented is appropriate to the user and that decisions, 
-          tasks, processes, and outcomes are easily understood. Links to other useful and relevant content (for example, related pages, external 
-          websites, or documents) are available and shown in context.</p>
-        <p>Requirement: prompt users to explain terminology in their own words to validate comprehension on key processes Observing 
-          facial and physical reactions help signal areas of confusion, concern or delight and should be probed for more feedback.</p>
-        
-        <h4>Digital products are device-agnostic.</h4>
-        <p>Usability tests should include a range of devices and assistive technologies to ensure government services will perform in any 
-          technology setting users choose to use.</p>
-        <p>Requirement: test participants should use their preferred choice of technology during tests and usability research 
-          studies should include a range of devices.</p>
-        
-        <h4>Digital products are compliant.</h4>
-        <p>Usability tests should validate that we understand user needs and preferences around appropriate levels of trust, credibility, 
-          security, and privacy within our digital products.</p>
-        <p>Requirement: prompt users to describe their feelings, perceptions, or concerns regarding trust, security, and privacy.</p>
-        
-        <h4>Digital products are lean.</h4>
-        <p>Usability testing should be used to validate that enough features are available to meet user needs and that the experience does not 
-          include feature bloat making interfaces harder to understand or navigate.</p>
-        <p>Requirement: validate not just what features are needed but also what features are not. Features and functionality should 
-          only be developed when usability studies show users can easily complete their tasks using the proposed features and functionality.</p>
-        
-        <h4>Digital products are iterative.</h4>
-        <p>Usability testing should be done frequently and aligned to major releases or change in functionality. User needs evolve over time and 
-          continuous testing is required to ensure our experiences are keeping up with expectations.</p>
-        <p>Requirement: continuous testing and resource availability to act on findings. Tasks are validated and easily actionable 
-          on updated designs.</p>
-        
-        <h4>Digital products are connected.</h4>
-        <p>Usability testing should mirror the true end-to-end experience whenever possible so that we can ensure seamless experiences across channels. 
-          Features and functionality should support users desired workflows identified in usability testing.</p>
-        <p>Requirement: don’t just test the application, test the entire process. Workflows should be visualized with areas of 
-          friction and opportunity.</p>
-        
-        <h4>Digital products are responsive.</h4>
-        <p>Usability testing should validate that appropriate support is available within an application to assist users when they need help 
-          or want to provide feedback.</p>
-        <p>Requirement: support mechanisms should be part of usability test tasks.</p>
+
+        <h3>Guidelines:</h3>
+
+        <ol>
+          <li><strong>Accessible:&nbsp;</strong>Digital products will be usable for the broadest range of users possible regardless of physical 
+            or cognitive limitations, literacy level, or technical capability.</li>
+          <li><strong>Compliant:&nbsp;</strong>The product must leverage the brand and convey authority and legitimacy. As a government asset, 
+            user expectations of safety and security must be understood and met with experiences that evoke a sense of trust. </li>
+          <li><strong>Device-agnostic:&nbsp;</strong>Users should not be limited by the device they choose to use or the technology they have 
+            access to when interacting with government. </li>
+          <li><strong>Usable:&nbsp;</strong>Using human-centered design to understand users’ context of use, goals, tasks, and environments. </li>
+          <li><strong>Suitable:&nbsp;</strong>Users can achieve their goals and complete tasks in a manner that is satisfactory and will be 
+            on par with outside modern digital products used in other aspects of users lives. </li>
+          <li><strong>Connected:&nbsp;</strong>Digital products will be designed for integration into the broader eco-system of the government 
+            reducing the need for new and separate destinations that fracture the experience. </li>
+          <li><strong>Comprehensible:&nbsp;</strong>Digital products are broadly comprehensible by the largest user base possible by presenting 
+            information in plain language, absent of organizational speak, industry jargon, and complex legalese. </li>
+          <li><strong>Lean:&nbsp;</strong>Interfaces are designed to be the least amount of function possible to meet the needs of users in a 
+            way that feels intuitive and can be operated without the need of outside help.  </li>
+          <li><strong>Iterative:&nbsp;</strong>Products are continuously tested, resourced, and improved upon with users input and usage data 
+            to ensure lasting effectiveness, efficiency, and usability. </li>
+          <li><strong>Responsive:&nbsp;</strong>Digital products will make available and clearly visible channels for technical support and 
+            allow users to submit feedback on product performance, bugs, or suggest improvements. </li>
+        </ol>
+
+        <p>
+          For more details on the process of assessing compliance to each guideline, refer to our
+          <a href="https://goa-dio.atlassian.net/wiki/spaces/DIO/pages/3023569024/User+Experience+Guideline+Review+Process" target="_blank">
+            user experience guideline review process
+          </a>
+          , and
+          <a href="https://goa-dio.atlassian.net/wiki/spaces/DIO/pages/3023143415/Heuristic+evaluation+template" target="_blank">
+             heuristic evaluation template
+          </a>
+          for teams.
+        </p>
         
       </div>
     </>
