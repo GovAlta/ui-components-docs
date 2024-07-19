81266327582cec8e272f5361a2a140f7a9438137
Dustin Nielsen <nielsen.dustin@gmail.com>
2024-07-19 10:52:06 -0600

    chore: Adding a new Bug Verification page

---
+++ src/routes/get-started/developers/BugVerification.tsx
@@ -0,0 +1,9 @@
+import { ComponentContent } from "@components/component-content/ComponentContent";
+
+export default function BugVerificationPage() {
+  return (
+    <ComponentContent tocCssQuery="h2[id], h3[id]">
+      <h1>Bug Verification Process</h1>
+    </ComponentContent>
+  );
+}
