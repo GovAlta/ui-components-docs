3e5bd5f56cdbdc9eefdeb0ee42c9d68f62522a6b
mxsoco <miguel.soco@gov.ab.ca>
2024-10-15 14:38:05 -0600

    chore: Update Layout.tsx

--- src/routes/foundations/Layout.tsx
+++ src/routes/foundations/Layout.tsx
@@ -1,13 +1,18 @@
+import { GoABlock, GoADivider, GoAFormItem, GoAGrid, GoASideMenu } from "@abgov/react-components";
 import { ComponentContent } from "@components/component-content/ComponentContent.tsx";
 
 export default function FoundationsLayoutPage() {
   return (
+    
       <ComponentContent>
         <h1>Layout</h1>
         <h3>We use the layout as a structural template to support consistency across our services. By defining visual grids, spacing, and sections, we create intuitive services for our users.</h3>
         <GoADivider mt="3xl" mb="3xl"></GoADivider>
         <h3>The spacing scale</h3>
         <p>The Design System uses a spacing scale with a 16px base value to be used within layout spacing and spacing within components. See spacing for more information.</p>
+
+        <h2>Breakpoints</h2>
+        <p>Default breakpoint sizing for different viewport widths are as follows:</p>
       </ComponentContent>
   );
 }
