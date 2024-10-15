44627f996e47e05399f409a251feeb6ed53c7c3e
mxsoco <miguel.soco@gov.ab.ca>
2024-10-15 15:18:51 -0600

    chore: Update Layout.tsx

--- src/routes/foundations/Layout.tsx
+++ src/routes/foundations/Layout.tsx
@@ -1,4 +1,4 @@
-import { GoADivider, GoATable } from "@abgov/react-components";
+import { GoADivider, GoATable, GoABlock } from "@abgov/react-components";
 import { ComponentContent } from "@components/component-content/ComponentContent.tsx";
 
 export default function FoundationsLayoutPage() {
@@ -7,11 +7,11 @@ export default function FoundationsLayoutPage() {
       <ComponentContent>
         <h1>Layout</h1>
         <h3>We use the layout as a structural template to support consistency across our services. By defining visual grids, spacing, and sections, we create intuitive services for our users.</h3>
-        <GoADivider mt="3xl" mb="xl"></GoADivider>
+        <GoADivider mt="2xl" mb="2xl"></GoADivider>
         <h3>The spacing scale</h3>
         <p>The Design System uses a spacing scale with a 16px base value to be used within layout spacing and spacing within components. See spacing for more information.</p>
 
-        <GoADivider mt="xl" mb="xl"></GoADivider>
+        <GoADivider mt="2xl" mb="2xl"></GoADivider>
         <h2>Breakpoints</h2>
         <p>Default breakpoint sizing for different viewport widths are as follows:</p>
         <GoATable width="100%" variant="relaxed">
@@ -64,12 +64,67 @@ export default function FoundationsLayoutPage() {
             </tr>
           </tbody>
         </GoATable>
-        
-        <GoADivider mt="xl" mb="xl"></GoADivider>
         <h2>Margins</h2>
         <h3>Default margin</h3>
         <p>Margins provide a visual buffer between a view’s content and any content outside of the view’s bounds. These inset values create a space between the edges of the view’s bounds rectangle and the content inside the view.</p>
-          
+
+        <GoABlock mt="xl" mb="xl">
+        <p><em>placeholder</em></p>
+        </GoABlock>
+        
+        <GoADivider mt="2xl" mb="2xl"></GoADivider>
+
+        <h2>Common layouts</h2>
+        <h3>Basic form layout</h3>
+        <p>A form layout uses a 2/3 content width with a default maximum page width of 960px. This ensures optimal line length (50-75 characters) and simplifies the content for the user. This layout is often used for external, citizen-facing forms.</p>
+        
+        <GoABlock mt="xl" mb="xl">
+        <p><em>placeholder</em></p>
+        </GoABlock>
+        
+        <h3>Basic form layout example</h3>
+        <p>This layout is recommended for citizen facing applications where the content is simplified, and often broken down into one question per page.</p>
+        
+        <GoABlock mt="xl" mb="xl">
+        <p><em>placeholder</em></p>
+        </GoABlock>
+
+        <GoADivider mt="2xl" mb="2xl"></GoADivider>
+        
+        <h3>Side navigation layout</h3>
+        <p>A 2 column layout that has a fixed side navigation included by default with a fluid content container.</p>
+        
+        <GoABlock mt="xl" mb="xl">
+        <p><em>placeholder</em></p>
+        </GoABlock>
+        
+        <p>When designing for large screens, consider a max content width of 1464px to prevent the content from getting too wide.</p>
+        <GoABlock mt="xl" mb="xl">
+        <p><em>placeholder</em></p>
+        </GoABlock>
+        
+        <h3>Side navigation layout example</h3>
+        <p>A 2 column layout is often used for internal applications, where there is a need for a side navigation. </p>
+        
+        <GoABlock mt="xl" mb="xl">
+        <p><em>placeholder</em></p>
+        </GoABlock>
+        
+        <GoADivider mt="2xl" mb="2xl"></GoADivider>
+        
+        <h3>Side navigation and side bar layout</h3>
+        <p>A 3 column layout that has a fixed side navigation as well as a second side panel on the right hand side. The third column is often used to show tertiary information, comments, or other documents alongside the main content area. Depending on your content, for large viewports it is recommended to use a max content width of 1176px on the main content area.</p>
+        
+        <GoABlock mt="xl" mb="xl">
+        <p><em>placeholder</em></p>
+        </GoABlock>
+        
+        <h3>Side navigation and sidebar layout example</h3>
+        <p>A 3 column layout is often used for internal applications, where there is a need for both a side navigation as well as an additional section for notes or comments. </p>
+        
+        <GoABlock mt="xl" mb="xl">
+        <p><em>placeholder</em></p>
+        </GoABlock>
       </ComponentContent>
   );
 }
