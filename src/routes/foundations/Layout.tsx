206b5592569100cfe5a77d42ccef7ca46a71c60d
mxsoco <miguel.soco@gov.ab.ca>
2024-10-15 14:32:00 -0600

    chore: Update Layout.tsx

--- src/routes/foundations/Layout.tsx
+++ src/routes/foundations/Layout.tsx
@@ -4,6 +4,10 @@ export default function FoundationsLayoutPage() {
   return (
       <ComponentContent>
         <h1>Layout</h1>
+        <h3>We use the layout as a structural template to support consistency across our services. By defining visual grids, spacing, and sections, we create intuitive services for our users.</h3>
+        <GoADivider mt="3xl" mb="3xl"></GoADivider>
+        <h3>The spacing scale</h3>
+        <p>The Design System uses a spacing scale with a 16px base value to be used within layout spacing and spacing within components. See spacing for more information.</p>
       </ComponentContent>
   );
 }
