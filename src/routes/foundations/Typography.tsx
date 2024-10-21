34ff35616ef0e9a468d8c1682b74f0733976caa0
mxsoco <miguel.soco@gov.ab.ca>
2024-10-21 12:54:58 -0600

    chore: Update Typography.tsx

--- src/routes/foundations/Typography.tsx
+++ src/routes/foundations/Typography.tsx
@@ -1,9 +1,13 @@
+import { GoADivider } from "@abgov/react-components";
 import { ComponentContent } from "@components/component-content/ComponentContent.tsx";
 
 export default function FoundationsTypographyPage() {
   return (
       <ComponentContent>
         <h1>Typography</h1>
+         <GoADivider mt="2xl" mb="2xl"></GoADivider>
+
+        <h2>Importance of visual hierarchy</h2>
       </ComponentContent>
   );
 }
