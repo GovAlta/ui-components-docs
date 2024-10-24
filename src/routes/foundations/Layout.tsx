908a89b83de7f21e9f341a02190e7dfbd26c8a56
mxsoco <miguel.soco@gov.ab.ca>
2024-10-24 11:13:47 -0600

    chore: Update Layout.tsx from local

--- src/routes/foundations/Layout.tsx
+++ src/routes/foundations/Layout.tsx
@@ -1,163 +1,148 @@
-import { GoADivider, GoATable, GoABlock } from "@abgov/react-components";
-import { DoDont } from "@components/do-dont/DoDont";
+import { GoADivider, GoATable, GoAContainer } from "@abgov/react-components";
 import { ComponentContent } from "@components/component-content/ComponentContent.tsx";
 
 export default function FoundationsLayoutPage() {
   return (
     
-      <ComponentContent>
+      <ComponentContent tocCssQuery="h2[id], h3[id]">
         <h1>Layout</h1>
         <h3>We use the layout as a structural template to support consistency across our services. By defining visual grids, spacing, and sections, we create intuitive services for our users.</h3>
         <GoADivider mt="2xl" mb="2xl"></GoADivider>
-        <h3>The spacing scale</h3>
-        <p>The Design System uses a spacing scale with a 16px base value to be used within layout spacing and spacing within components. See spacing for more information.</p>
+        <div className="max-width-72ch">
+          <h3 id="spacing-scale">The spacing scale</h3>
+          <p>The Design System uses a spacing scale with a 16px base value to be used within layout spacing and spacing within components. See spacing for more information.</p>
 
-        <GoADivider mt="2xl" mb="2xl"></GoADivider>
-        <h2>Breakpoints</h2>
-        <p>Default breakpoint sizing for different viewport widths are as follows:</p>
-        <GoATable width="100%" variant="relaxed">
-          <thead>
-            <tr>
-              <th>
-                Breakpoint
-              </th>
-              <th>
-                Viewport(px)
-              </th>
-              <th>
-                Margin
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                Small screen (mobile)
-              </td>
-              <td>
-                &lt;624px
-              </td>
-              <td>
-                16px
-              </td>
-            </tr>
-            <tr>
-              <td>
-                Medium screen (tablet)
-              </td>
-              <td>
-                624-1024px
-              </td>
-              <td>
-                32px
-              </td>
-            </tr>
-            <tr>
-              <td>
-                Large screen (desktop)
-              </td>
-              <td>
-                &gt;1024px
-              </td>
-              <td>
-                64px
-              </td>
-            </tr>
-          </tbody>
-        </GoATable>
-        <h2>Margins</h2>
-        <h3>Default margin</h3>
-        <p>Margins provide a visual buffer between a view’s content and any content outside of the view’s bounds. These inset values create a space between the edges of the view’s bounds rectangle and the content inside the view.</p>
+          <GoADivider mt="2xl" mb="2xl"></GoADivider>
+          <h2 id="breakpoints">Breakpoints</h2>
+          <p>Default breakpoint sizing for different viewport widths are as follows:</p>
+          <GoATable width="100%" variant="relaxed">
+            <thead>
+              <tr>
+                <th>
+                  Breakpoint
+                </th>
+                <th>
+                  Viewport(px)
+                </th>
+                <th>
+                  Margin
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  Small screen (mobile)
+                </td>
+                <td>
+                  &lt;624px
+                </td>
+                <td>
+                  16px
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  Medium screen (tablet)
+                </td>
+                <td>
+                  624-1024px
+                </td>
+                <td>
+                  32px
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  Large screen (desktop)
+                </td>
+                <td>
+                  &gt;1024px
+                </td>
+                <td>
+                  64px
+                </td>
+              </tr>
+            </tbody>
+          </GoATable>
+          <h2 id="margins">Margins</h2>
+          <h3 id="default-margin">Default margin</h3>
+          <p>Margins provide a visual buffer between a view's content and any content outside of the view's bounds. These inset values create a space between the edges of the view’s bounds rectangle and the content inside the view.</p>
 
-        <GoABlock mt="xl" mb="xl">
-          <DoDont type="generic">
-            <div style={{ textAlign: "center" }}>
-              <img src="/images/layout/default-margins.png" width="80%"></img>
-            </div>
-          </DoDont>
-        </GoABlock>
-        
-        <GoADivider mt="2xl" mb="2xl"></GoADivider>
+          <GoAContainer mt="xl" mb="xl">
+              <div style={{ textAlign: "center" }}>
+                <img src="/images/layout/default-margins.png" width="100%"></img>
+              </div>
+          </GoAContainer>
+          
+          <GoADivider mt="2xl" mb="2xl"></GoADivider>
 
-        <h2>Common layouts</h2>
-        <h3>Basic form layout</h3>
-        <p>A form layout uses a 2/3 content width with a default maximum page width of 960px. This ensures optimal line length (50-75 characters) and simplifies the content for the user. This layout is often used for external, citizen-facing forms.</p>
-        
-        <GoABlock mt="xl" mb="xl">
-          <DoDont type="generic">
-            <div style={{ textAlign: "center" }}>
-              <img src="/images/layout/basic-form-layout.png" width="80%"></img>
-            </div>
-          </DoDont>
-        </GoABlock>
-        
-        <h3>Basic form layout example</h3>
-        <p>This layout is recommended for citizen facing applications where the content is simplified, and often broken down into one question per page.</p>
-        
-        <GoABlock mt="xl" mb="xl">
-          <DoDont type="generic">
-            <div style={{ textAlign: "center" }}>
-              <img src="/images/layout/basic-form-layout-example.png" width="80%"></img>
-            </div>
-          </DoDont>
-        </GoABlock>
+          <h2 id="common-layouts">Common layouts</h2>
+          <h3 id="basic-form-layout">Basic form layout</h3>
+          <p>A form layout uses a 2/3 content width with a default maximum page width of 960px. This ensures optimal line length (50-75 characters) and simplifies the content for the user. This layout is often used for external, citizen-facing forms.</p>
+          
+          <GoAContainer mt="xl" mb="xl">
+              <div style={{ textAlign: "center" }}>
+                <img src="/images/layout/basic-form-layout.png" width="100%"></img>
+              </div>
+          </GoAContainer>
+          
+          <h3 id="basic-form-template">Basic form layout example</h3>
+          <p>This layout is recommended for citizen facing applications where the content is simplified, and often broken down into one question per page.</p>
+          
+          <GoAContainer mt="xl" mb="xl">
+              <div style={{ textAlign: "center" }}>
+                <img src="/images/layout/basic-form-layout-example.png" width="100%"></img>
+              </div>
+          </GoAContainer>
 
-        <GoADivider mt="2xl" mb="2xl"></GoADivider>
-        
-        <h3>Side navigation layout</h3>
-        <p>A 2 column layout that has a fixed side navigation included by default with a fluid content container.</p>
-        
-        <GoABlock mt="xl" mb="xl">
-          <DoDont type="generic">
-            <div style={{ textAlign: "center" }}>
-              <img src="/images/layout/side-nav-layout.png" width="80%"></img>
-            </div>
-          </DoDont>
-        </GoABlock>
-        
-        <p>When designing for large screens, consider a max content width of 1464px to prevent the content from getting too wide.</p>
-        <GoABlock mt="xl" mb="xl">
-          <DoDont type="generic">
-            <div style={{ textAlign: "center" }}>
-              <img src="/images/layout/side-nav-layout-two.png" width="80%"></img>
-            </div>
-          </DoDont>
-        </GoABlock>
-        
-        <h3>Side navigation layout example</h3>
-        <p>A 2 column layout is often used for internal applications, where there is a need for a side navigation. </p>
-        
-        <GoABlock mt="xl" mb="xl">
-          <DoDont type="generic">
-            <div style={{ textAlign: "center" }}>
-              <img src="/images/layout/side-nav-layout-example.png" width="80%"></img>
-            </div>
-          </DoDont>
-        </GoABlock>
-        
-        <GoADivider mt="2xl" mb="2xl"></GoADivider>
-        
-        <h3>Side navigation and side bar layout</h3>
-        <p>A 3 column layout that has a fixed side navigation as well as a second side panel on the right hand side. The third column is often used to show tertiary information, comments, or other documents alongside the main content area. Depending on your content, for large viewports it is recommended to use a max content width of 1176px on the main content area.</p>
-        
-        <GoABlock mt="xl" mb="xl">
-          <DoDont type="generic">
-            <div style={{ textAlign: "center" }}>
-              <img src="/images/layout/side-nav-side-bar-layout.png" width="80%"></img>
-            </div>
-          </DoDont>
-        </GoABlock>
-        
-        <h3>Side navigation and sidebar layout example</h3>
-        <p>A 3 column layout is often used for internal applications, where there is a need for both a side navigation as well as an additional section for notes or comments. </p>
-        
-        <GoABlock mt="xl" mb="xl">
-          <DoDont type="generic">
-            <div style={{ textAlign: "center" }}>
-              <img src="/images/layout/side-nav-side-bar-layout-example.png" width="80%"></img>
-            </div>
-          </DoDont>
-        </GoABlock>
+          <GoADivider mt="2xl" mb="2xl"></GoADivider>
+          
+          <h3 id="side-nav-layout">Side navigation layout</h3>
+          <p>A 2 column layout that has a fixed side navigation included by default with a fluid content container.</p>
+          
+          <GoAContainer mt="xl" mb="xl">
+              <div style={{ textAlign: "center" }}>
+                <img src="/images/layout/side-nav-layout.png" width="100%"></img>
+              </div>
+          </GoAContainer>
+          
+          <p>When designing for large screens, consider a max content width of 1464px to prevent the content from getting too wide.</p>
+          <GoAContainer mt="xl" mb="xl">
+              <div style={{ textAlign: "center" }}>
+                <img src="/images/layout/side-nav-layout-two.png" width="100%"></img>
+              </div>
+          </GoAContainer>
+          
+          <h3 id="side-nav-example">Side navigation layout example</h3>
+          <p>A 2 column layout is often used for internal applications, where there is a need for a side navigation. </p>
+          
+          <GoAContainer mt="xl" mb="xl">
+              <div style={{ textAlign: "center" }}>
+                <img src="/images/layout/side-nav-layout-example.png" width="100%"></img>
+              </div>
+          </GoAContainer>
+          
+          <GoADivider mt="2xl" mb="2xl"></GoADivider>
+          
+          <h3 id="side-nav-side-bar">Side navigation and side bar layout</h3>
+          <p>A 3 column layout that has a fixed side navigation as well as a second side panel on the right hand side. The third column is often used to show tertiary information, comments, or other documents alongside the main content area. Depending on your content, for large viewports it is recommended to use a max content width of 1176px on the main content area.</p>
+          
+          <GoAContainer mt="xl" mb="xl">
+              <div style={{ textAlign: "center" }}>
+                <img src="/images/layout/side-nav-side-bar-layout.png" width="100%"></img>
+              </div>
+          </GoAContainer>
+          
+          <h3 id="side-nav-side-bar-example">Side navigation and sidebar layout example</h3>
+          <p>A 3 column layout is often used for internal applications, where there is a need for both a side navigation as well as an additional section for notes or comments. </p>
+          
+          <GoAContainer mt="xl" mb="xl">
+              <div style={{ textAlign: "center" }}>
+                <img src="/images/layout/side-nav-side-bar-layout-example.png" width="100%"></img>
+              </div>
+          </GoAContainer>
+        </div>
       </ComponentContent>
   );
 }
