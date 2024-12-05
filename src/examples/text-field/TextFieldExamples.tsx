bb2f8a52cff99a7f64b9af416333941ce4f9ec07
miguel-soco_goa <youremail@gov.ab.ca>
2024-12-05 16:27:33 -0700

    chore(#2137): input - add in contextual example

--- src/examples/text-field/TextFieldExamples.tsx
+++ src/examples/text-field/TextFieldExamples.tsx
@@ -105,6 +105,100 @@ export default function TextFieldExamples() {
         </GoABlock>
       </Sandbox>
 
+      <h3 id="component-example-enter-phone-number">
+        Enter a phone number
+      </h3>
+      <Sandbox flags={["reactive"]} skipRender>
+        
+        {/*Angular Code Snippet - need for leadingContent slot*/}
+        <CodeSnippet
+          lang="typescript"
+          tags="angular"
+          allowCopy={true}
+          code={`
+                  export class ExampleComponent {
+                    phoneNumber: number|undefined;
+
+                    onChangePhoneNumber(event: Event) {
+                      this.phoneNumber = (event as CustomEvent).detail.value as number;
+                    }
+                  }
+                `}
+        />
+        <CodeSnippet
+          lang="html"
+          tags="angular"
+          allowCopy={true}
+          code={`
+                  <goa-form-item label="Phone number">
+                    <goa-input (_change)="onChangePhoneNumber($event)" value="" name="phoneNumber" type="tel" mb="xl">
+                      <div slot="leadingContent">$</div>
+                    </goa-input>
+                  </goa-form-item>
+                `}
+        />
+
+        {/*Reactive FormControl Angular Code Snippet*/}
+        <CodeSnippet
+          lang="typescript"
+          tags={["angular", "reactive"]}
+          allowCopy={true}
+          code={`
+                  export class ExampleComponent {
+                    phoneNumberForm = new FormGroup({
+                      phoneNumber: new FormControl(""),
+                    });
+                  }  
+                `}
+        />
+        <CodeSnippet
+          lang="html"
+          tags={["angular", "reactive"]}
+          allowCopy={true}
+          code={`
+                <form [formGroup]="phoneNumberForm">
+                  <goa-form-item label="Phone number">
+                      <goa-input goaValue name="phoneNumber" formControlName="phoneNumber" type="tel">
+                        <div slot="leadingContent">+1</div>
+                      </goa-input>
+                  </goa-form-item>                
+                </form>
+                `}
+        />
+
+        {/*React Code Snippet - need for leadingContent slot*/}
+        <CodeSnippet
+          lang="typescript"
+          tags="react"
+          allowCopy={true}
+          code={`
+                  const [phoneNumber, setPhoneNumber] = useState<string>("");
+                  function onChangePhoneNumber(_name: string, value: string) {
+                    setPhoneNumber(value);
+                  }
+                `}
+        />
+        <CodeSnippet
+          lang="typescript"
+          tags="react"
+          allowCopy={true}
+          code={`
+                  <GoAFormItem label="Phone number">
+                    <GoAInput onChange={onChangePhoneNumber} value={phoneNumber} name="phoneNumber" type="tel" leadingContent="+1"></GoAInput>
+                  </GoAFormItem>
+                `}
+        />
+        <GoAFormItem label="Phone number">
+          <GoAInput
+            onChange={noop}
+            value=""
+            type="tel"
+            name="phoneNumber"
+            leadingContent="+1"
+          />
+        </GoAFormItem>
+      </Sandbox>
+
       <h3 id="component-example-ask-for-costs">Ask a user for dollar amounts or costs</h3>
       <Sandbox flags={["reactive"]} skipRender>
         {/*Angular Code Snippet - need for leadingContent slot*/}
@@ -269,6 +363,41 @@ export default function TextFieldExamples() {
                 value=""
                 name="position"
                 width="71px"
+              />
+            </GoAFormItem>
+          </GoABlock>
+        </GoAFormItem>
+      </Sandbox>
+
+      <h3 id="component-example-ask-user-for-bank-details">
+        Ask a user for bank details
+      </h3>
+      <Sandbox flags={["reactive"]} 
+      fullWidth>
+        <GoAFormItem label="Bank account details" labelSize="large">
+          <GoABlock gap="m" direction="column">
+            <GoAFormItem label="Name on account">
+              <GoAInput
+                onChange={noop}
+                value=""
+                name="bank-details"
+                width="100%"
+              />
+            </GoAFormItem>
+            <GoAFormItem label="Account number">
+              <GoAInput
+                onChange={noop}
+                value=""
+                name="acount-number"
+              />
+            </GoAFormItem>
+            <GoAFormItem label="Bank number" helpText="Must be between 6 and 8 digits long">
+              <GoAInput
+                onChange={noop}
+                maxLength={8}
+                type={"text"}
+                value=""
+                name="bank-number"
               />
             </GoAFormItem>
           </GoABlock>
