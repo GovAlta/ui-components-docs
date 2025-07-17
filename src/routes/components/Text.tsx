20ae8201f39240c63ddb2642671028554226b20d
Benji Franck <benjamin.franck@gmail.com>
2025-07-17 16:27:00 -0600

    fix(#2440): Add ID property to Text component

--- src/routes/components/Text.tsx
+++ src/routes/components/Text.tsx
@@ -59,7 +59,7 @@ export default function TextPage() {
     },
   ]);
 
-  const componentProperties: ComponentProperty[] = [
+    const oldComponentProperties: ComponentProperty[] = [
     {
       name: "tag",
       type: "h1 | h2 | h3 | h4 | h5 | span | div | p",
@@ -90,6 +90,42 @@ export default function TextPage() {
     },
   ];
 
+  const componentProperties: ComponentProperty[] = [
+    {
+      name: "tag",
+      type: "h1 | h2 | h3 | h4 | h5 | span | div | p",
+      description: "Sets the tag and text size.",
+      defaultValue: "div"
+    },
+    {
+      name: "size",
+      type: "heading-xl | heading-l | heading-m | heading-s | heading-xs | body-l | body-m | body-s | body-xs",
+      description: "Overrides the text size from the 'as' property."
+    },
+    {
+     name: "maxWidth",
+     type: "string",
+     description: "Sets the max width.",
+     defaultValue: "65ch"
+    },
+     {
+      name: "color",
+      type: "primary | secondary",
+      description: "Sets the text colour.",
+      defaultValue: "primary"
+     },
+    {
+      name: "mt,mr,mb,ml",
+      type: "none | 3xs | 2xs | xs | s | m | l | xl | 2xl | 3xl | 4xl",
+      description: "Apply margin to the top, right, bottom, and/or left of the component.",
+    },
+    {
+      name: "id",
+      type: "string",
+      description: "Sets the HTML id attribute on the text element.",
+    },
+  ];
+
   function onSandboxChange(bindings: ComponentBinding[], props: Record<string, unknown>) {
     setTextBindings(bindings);
     setTextProps(props);
@@ -114,7 +150,7 @@ export default function TextPage() {
               dolore magna aliqua.
             </GoabText>
           </Sandbox>
-          <ComponentProperties properties={componentProperties} />
+          <ComponentProperties properties={componentProperties} oldProperties={oldComponentProperties} />
         </GoabTab>
 
         <GoabTab
