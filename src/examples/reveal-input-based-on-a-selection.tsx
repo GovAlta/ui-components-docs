e9930b40d0ea893fb87f913d2f34765209690116
Thomas Jeffery <thomaswjeffery@gmail.com>
2025-08-14 17:48:41 -0600

    Move reveal components to new directory structure and fix imports

--- src/examples/reveal-input-based-on-a-selection.tsx
+++ src/examples/reveal-input-based-on-a-selection.tsx
@@ -1,6 +1,6 @@
-import { RevealRadioInputBasedOnSelection } from "@examples/reveal-radio-input-based-on-selection.tsx";
+import { RadioRevealSlotExample } from "@examples/radio/RadioRevealSlotExample.tsx";
 import { SandboxHeader } from "@components/sandbox/sandbox-header/sandboxHeader.tsx";
-import { RevealCheckboxInputBasedOnSelection } from "@examples/reveal-checkbox-input-based-on-selection.tsx";
+import { CheckboxRevealSlotExample } from "@examples/checkbox/CheckboxRevealSlotExample.tsx";
 
 export default function RevealInputBasedOnASelection() {
 
@@ -10,13 +10,13 @@ export default function RevealInputBasedOnASelection() {
         exampleTitle="Reveal a checkbox input based on a selection"
         figmaExample="https://www.figma.com/design/aIRjvBzpIUH0GbkffjbL04/%E2%9D%96-Patterns-library-%7C-DDD?node-id=6307-131333&t=i24jD5GZOelxxzSN-4">
       </SandboxHeader>
-      <RevealCheckboxInputBasedOnSelection />
+      <CheckboxRevealSlotExample />
 
       <SandboxHeader
         exampleTitle="Reveal a radio input based on a selection"
         figmaExample="https://www.figma.com/design/aIRjvBzpIUH0GbkffjbL04/%E2%9D%96-Patterns-library-%7C-DDD?node-id=6307-131333&t=i24jD5GZOelxxzSN-4">
       </SandboxHeader>
-      <RevealRadioInputBasedOnSelection />
+      <RadioRevealSlotExample />
     </>
   );
 }
